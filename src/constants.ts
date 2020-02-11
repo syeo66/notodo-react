c416ae76fa464c0391938808606a9273bc6945f7
Red Ochsenbein <red@redochsenbein.ch>
2020-02-11 21:52:31 +0100

    Add local proxy for development, connect to tonotodo.com/graphql to prevent cors pre flight

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
 export const AUTH_TOKEN = 'authToken'
 
-export const GRAPHQL_API = 'https://api.tonotodo.com/graphql'
+export const GRAPHQL_API = process.env.REACT_APP_API_URL || 'https://tonotodo.com/graphql'
 
 export const DATE_FORMAT = 'dd.MM.yyyy'
 export const TIME_FORMAT = 'HH:mm'
