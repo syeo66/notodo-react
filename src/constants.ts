d044f0340a1ff15db60afd4c2c1b45490de3f231
Red Ochsenbein <red@redochsenbein.ch>
2020-02-13 21:52:03 +0100

    Add day of week to DATE_FORMAT

--- src/constants.ts
+++ src/constants.ts
@@ -2,5 +2,5 @@ export const AUTH_TOKEN = 'authToken'
 
 export const GRAPHQL_API = process.env.REACT_APP_API_URL || 'https://tonotodo.com/graphql'
 
-export const DATE_FORMAT = 'dd.MM.yyyy'
+export const DATE_FORMAT = 'eeeeee, dd.MM.yyyy'
 export const TIME_FORMAT = 'HH:mm'
