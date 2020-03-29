72b0f19939a7d53cd3e7f7904d2544d7b4ab9c0f
Red Ochsenbein <red@redochsenbein.ch>
2020-03-29 10:36:32 +0200

    Make proxy target configurable using env variable

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,12 +2,14 @@
 const { createProxyMiddleware } = require('http-proxy-middleware')
 
 let cookie
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 function relayRequestHeaders(proxyReq, req) {
   if (cookie) {
     proxyReq.setHeader('cookie', cookie)
   }
 }
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 function relayResponseHeaders(proxyRes, req, res) {
   const proxyCookie = proxyRes.headers['set-cookie']
   if (proxyCookie) {
@@ -22,12 +24,13 @@ module.exports = function(app) {
       autoRewrite: true,
       changeOrigin: true,
       cookieDomainRewrite: '',
-      headers: { host: 'tonotodo.com' },
+      headers: { host: process.env.REACT_APP_API_TARGET_HOST || 'tonotodo.com' },
       hostRewrite: 'localhost',
       onProxyReq: relayRequestHeaders,
       onProxyRes: relayResponseHeaders,
       secure: false,
-      target: 'https://tonotodo.com',
+      target: `${process.env.REACT_APP_API_TARGET_PROTOCOL || 'https'}://${process.env.REACT_APP_API_TARGET_HOST ||
+        'tonotodo.com'}`,
     })
   )
 }
